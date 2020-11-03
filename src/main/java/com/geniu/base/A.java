505dd4be927442a204eb46dcbe88ce28cdaeb3ac
zhongshibo <z20107824@163.com>
2020-11-03 13:41:14 +0800

    拼接字符串几种方式的比较

---
+++ src/main/java/com/geniu/base/A.java
@@ -0,0 +1,7 @@
+package com.geniu.base;
+
+public class A {
+	public void log() {
+		System.out.println("log function executing");
+	}
+}
---
+++ src/main/java/com/geniu/base/B.java
@@ -0,0 +1,8 @@
+package com.geniu.base;
+
+public class B {
+	public void call(A a) {
+		//do something
+		a = null;
+	}
+}
