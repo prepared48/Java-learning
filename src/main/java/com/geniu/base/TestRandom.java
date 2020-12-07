b774a5db2ace26cbfc3896f59977d792b1a340bd
zhongshibo <z20107824@163.com>
2020-12-07 17:16:02 +0800

    增加ForkJoin例子

--- src/main/java/com/geniu/base/TestRandom.java
+++ src/main/java/com/geniu/base/TestRandom.java
@@ -12,7 +12,26 @@ import java.math.BigDecimal;
 public class TestRandom {
 
     public static void main(String[] args) {
-        float Max = 2, Min = 0;
+//        generateRandom(20, 60);
+//        System.out.println(sum / 10.0);
+        System.out.println(generateSixRandomNum());
+    }
+
+    /**
+     * 生成6位随机整数
+     *
+     * @return
+     */
+    public static int generateSixRandomNum() {
+        return (int) ((Math.random() * 9 + 1) * 100000);
+    }
+
+    /**
+     * 生成范围内随机消暑
+     *
+     * @return
+     */
+    public static void generateRandom(float Max, float Min) {
         float sum = 0;
         for (int i = 0; i < 24; i++) {
             BigDecimal db = new BigDecimal(Math.random() * (Max - Min) + Min);
@@ -21,6 +40,5 @@ public class TestRandom {
 //            sum += Float.parseFloat(db.setScale(2, BigDecimal.ROUND_HALF_UP)// 保留30位小数并四舍五入
 //                    .toString());
         }
-//        System.out.println(sum / 10.0);
     }
 }
