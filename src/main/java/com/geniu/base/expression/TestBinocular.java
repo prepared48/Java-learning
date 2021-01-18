f47ad810e56290c917f68c585efbe5273ee629f7
zhongshibo <z20107824@163.com>
2021-01-18 14:41:34 +0800

    增加注释

--- src/main/java/com/geniu/base/expression/TestBinocular.java
+++ src/main/java/com/geniu/base/expression/TestBinocular.java
@@ -1,7 +1,26 @@
-package com.geniu.base.expression;/**
- * 
+package com.geniu.base.expression;
+
+/**
+ * 测试三目表达式
+ * 运算符的优先级：+ > 三目运算符
+ * 所以如果以下三目运算符的目标是 计算 (a+b)和（c+d）
+ * 应该加括号
+ * （c==null?0L:c） + d
+ * 如果不加括号，相当于：c==null?0L:（c + d）
+ *
  * @Author: zhongshibo
  * @Date: 2021/1/18 14:26
  */
 public class TestBinocular {
+
+    public static void main(String[] args) {
+        Long a = 1L;
+        Long b = 163L;
+        System.out.println(a == null ? 0L : a + b);
+
+        Long c = null;
+        Long d = 163L;
+        System.out.println(c == null ? 0L : c + d);
+
+    }
 }
