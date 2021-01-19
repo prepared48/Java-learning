a791cede261abd8fdd13f2097964c66b75f639aa
zhongshibo <z20107824@163.com>
2021-01-19 17:46:26 +0800

    add list remove demo

---
+++ src/main/java/com/geniu/base/Polymorphism/TestEntity.java
@@ -0,0 +1,7 @@
+package com.geniu.base.Polymorphism;/**
+ * 
+ * @Author: zhongshibo
+ * @Date: 2021/1/19 09:26
+ */
+public class TestEntity {
+}
---
+++ src/main/java/com/geniu/collection/TestListRemove.java
@@ -0,0 +1,7 @@
+package com.geniu.collection;/**
+ * 
+ * @Author: zhongshibo
+ * @Date: 2021/1/19 16:49
+ */
+public class TestListRemove {
+}
