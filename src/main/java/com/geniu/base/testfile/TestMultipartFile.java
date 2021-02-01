70112e9b8b402b1b56879fce3abe550fcc1f2bf9
zhongshibo <z20107824@163.com>
2021-02-01 11:49:01 +0800

    add multipartFile contruct

---
+++ src/main/java/com/geniu/base/testfile/TestMultipartFile.java
@@ -0,0 +1,34 @@
+package com.geniu.base.testfile;
+
+import org.apache.http.entity.ContentType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+/**
+ * file 转成 MultipartFile（spring-web）
+ *
+ * @Author: zhongshibo
+ * @Date: 2020/11/18 14:44
+ */
+public class TestMultipartFile {
+
+    private static final String ROOT_FOLDER = new File("").getAbsolutePath();
+
+    public static void main(String[] args) {
+        String fileName = ROOT_FOLDER + "/file/demo.jpg";
+        File file = new File(fileName);
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            MultipartFile multipartFile = new MockMultipartFile(file.getName(), file.getName(),
+                    ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
+            System.out.println(multipartFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
