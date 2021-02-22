7762f6d62e2a55ecfa3ca0bcc4f135c263470ec7
shibo_zhong <z20107824@163.com>
2021-02-22 17:57:00 +0800

    thread visibility demo

---
+++ src/main/java/com/geniu/concurrent/javaConcurrencyInPractice/chapter3/Test0301NoVisibility.java
@@ -0,0 +1,31 @@
+package com.geniu.concurrent.javaConcurrencyInPractice.chapter3;
+
+/**
+ * 错误示范：在没有同步的情况下共享变量
+ *
+ * @Author: zhongshibo
+ * @Date: 2021/2/22 17:43
+ */
+public class Test0301NoVisibility {
+
+    private static boolean ready;
+
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+}
