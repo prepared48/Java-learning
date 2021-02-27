d3efc6c2fc7f625611e353a4c4a8b86cd8c61177
shibo_zhong <z20107824@163.com>
2021-02-27 22:52:19 +0800

    测试有界缓存

---
+++ src/main/java/com/geniu/concurrent/javaConcurrencyInPractice/chapter12/Test1201BoundedBuffer.java
@@ -0,0 +1,67 @@
+package com.geniu.concurrent.javaConcurrencyInPractice.chapter12;
+
+import org.omg.CORBA.Object;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 基于信号量的有界缓存
+ * 实际情况中，如果需要有界缓存，应该直接使用 ArrayBlockingQueue 或者 LinkedBlockingQueue
+ *
+ * @Author: zhongshibo
+ * @Date: 2021/2/27 21:56
+ */
+@ThreadSafe
+public class Test1201BoundedBuffer<E> {
+
+    // 可以从缓存中删除的元素个数 可以插入到缓存中的元素个数
+    private final Semaphore availableItems, availableSpaces;
+
+    private final E[] items;
+
+    private int putPosition = 0, takePosition = 0;
+
+    public Test1201BoundedBuffer(int capacity) {
+        availableItems = new Semaphore(0);
+        availableSpaces = new Semaphore(capacity);
+        items = (E[]) new Object[capacity];
+    }
+
+    public boolean isEmpty() {
+        return availableItems.availablePermits() == 0;
+    }
+
+    public boolean isFull() {
+        return availableSpaces.availablePermits() == 0;
+    }
+
+    public void put(E x) throws InterruptedException {
+        // 空间减1
+        availableSpaces.acquire();
+        doInsert(x);
+        // 数据允许+1
+        availableItems.release();
+    }
+
+    public E take() throws InterruptedException {
+        availableItems.acquire();
+        E item = doExtract();
+        availableSpaces.release();
+        return item;
+    }
+
+    private synchronized E doExtract() {
+        int i = takePosition;
+        E x = items[i];
+        items[i] = null;
+        takePosition = (++i == items.length) ? 0 : i;
+        return x;
+    }
+
+    private synchronized void doInsert(E x) {
+        int i = putPosition;
+        items[i] = x;
+        putPosition = (++i == items.length) ? 0 : i;
+    }
+}
---
+++ src/main/java/com/geniu/concurrent/javaConcurrencyInPractice/chapter12/Test1202BoundedBufferTest.java
@@ -0,0 +1,56 @@
+package com.geniu.concurrent.javaConcurrencyInPractice.chapter12;
+
+import junit.framework.TestCase;
+
+/**
+ * BoundedBuffer 的基本单元测试
+ *
+ * @Author: zhongshibo
+ * @Date: 2021/2/27 22:15
+ */
+public class Test1202BoundedBufferTest extends TestCase {
+
+    private static final long LOCKUP_DETECT_TIMEOUT = 10;
+
+    public void testIsEmptyWhenConstructed() {
+        Test1201BoundedBuffer<Integer> bb = new Test1201BoundedBuffer<>(10);
+        assertTrue(bb.isEmpty());
+        assertFalse(bb.isFull());
+    }
+
+    public void testIsFullAfterPuts() throws InterruptedException {
+        Test1201BoundedBuffer<Integer> bb = new Test1201BoundedBuffer<>(10);
+        for (int i = 0; i < 10; i++) {
+            bb.put(i);
+        }
+        assertTrue(bb.isFull());
+        assertFalse(bb.isEmpty());
+    }
+
+    /**
+     * 测试阻塞行为以及对中断的响应性
+     */
+    public void testTakeBlocksWhenEmpty() {
+        final Test1201BoundedBuffer<Integer> bb = new Test1201BoundedBuffer<>(10);
+        Thread taker = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    bb.take();
+                } catch (InterruptedException success) {
+                    // 一个空队列，获取对象就是应该报异常。成功
+                }
+            }
+        };
+
+        try {
+            taker.start();
+            Thread.sleep(LOCKUP_DETECT_TIMEOUT);
+            taker.interrupt();
+            taker.join(LOCKUP_DETECT_TIMEOUT);
+            assertFalse(taker.isAlive());
+        } catch (InterruptedException unexpected) {
+            fail();
+        }
+    }
+}
