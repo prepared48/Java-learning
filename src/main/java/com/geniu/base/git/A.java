5c0aa1fb587e75e2e2522ebe661a1269dfd29cc2
shibo_zhong <z20107824@163.com>
2021-03-09 10:32:32 +0800

    test git

---
+++ src/main/java/com/geniu/base/git/A.java
@@ -0,0 +1,12 @@
+package com.geniu.base.git;
+
+/**
+ * @Author: zhongshibo
+ * @Date: 2021/3/9 10:32
+ */
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
